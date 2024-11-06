e2f8ce597bd65d8ce22e77626c4c82ba1c989275
Jan Egil Ring <janegilring@microsoft.com>
2024-11-06 15:54:59 +0100

    Added test and customized test-result output locations
    Signed-off-by: Jan Egil Ring <janegilring@microsoft.com>

--- azure_arc_servers_jumpstart/artifacts/tests/Invoke-Test.ps1
+++ azure_arc_servers_jumpstart/artifacts/tests/Invoke-Test.ps1
@@ -39,9 +39,43 @@ switch ($env:flavor) {
 Write-Output "Tests succeeded: $tests_passed"
 Write-Output "Tests failed: $tests_failed"
 
-Write-Header "Adding deployment test results to wallpaper using BGInfo"
+#Write-Header "Adding deployment test results to wallpaper using BGInfo"
 
 Set-Content "$Env:windir\TEMP\arcbox-tests-succeeded.txt" $tests_passed
 Set-Content "$Env:windir\TEMP\arcbox-tests-failed.txt" $tests_failed
 
-bginfo.exe $Env:ArcBoxTestsDir\arcbox-bginfo.bgi /timer:0 /NOLICPROMPT
+#bginfo.exe $Env:ArcBoxTestsDir\arcbox-bginfo.bgi /timer:0 /NOLICPROMPT
+
+$DeploymentStatusPath = "C:\ArcBox\Logs\DeploymentStatus.log"
+
+Write-Header "Exporting deployment test results to $DeploymentStatusPath"
+
+Write-Output "Deployment Status" | Out-File -FilePath $DeploymentStatusPath
+
+Write-Output "`nTests succeeded: $tests_passed" | Out-File -FilePath $DeploymentStatusPath -Append
+Write-Output "Tests failed: $tests_failed`n" | Out-File -FilePath $DeploymentStatusPath -Append
+
+Write-Output "To get an updated deployment status, open Windows Terminal and run:" | Out-File -FilePath $DeploymentStatusPath -Append
+Write-Output "C:\ArcBox\Tests\Invoke-Test.ps1`n" | Out-File -FilePath $DeploymentStatusPath -Append
+
+Write-Output "Failed:" | Out-File -FilePath $DeploymentStatusPath -Append
+$tests_common.Failed | Out-File -FilePath $DeploymentStatusPath -Append
+$tests_k8s.Failed | Out-File -FilePath $DeploymentStatusPath -Append
+
+Write-Output "Passed:" | Out-File -FilePath $DeploymentStatusPath -Append
+$tests_k8s.Passed | Out-File -FilePath $DeploymentStatusPath -Append
+$tests_common.Passed | Out-File -FilePath $DeploymentStatusPath -Append
+
+Write-Header "Exporting deployment test results to resource group tag DeploymentStatus"
+
+$DeploymentStatusString = "Tests succeeded: $tests_passed Tests failed: $tests_failed"
+
+$tags = Get-AzResourceGroup -Name $env:resourceGroup | Select-Object -ExpandProperty Tags
+
+if ($null -ne $tags) {
+    $tags["DeploymentStatus"] = $DeploymentStatusString
+} else {
+    $tags = @{"DeploymentStatus" = $DeploymentStatusString}
+}
+
+$null = Set-AzResourceGroup -ResourceGroupName $env:resourceGroup -Tag $tags
--- azure_arc_servers_jumpstart/artifacts/tests/itpro.tests.ps1
+++ azure_arc_servers_jumpstart/artifacts/tests/itpro.tests.ps1
@@ -1,7 +1,7 @@
 
 BeforeDiscovery {
-    #$VMs = @("ArcBox-Ubuntu-01","ArcBox-Win2K19")
-    $VMs = @("ArcBox-Ubuntu-01")
+    $VMs = @("ArcBox-Ubuntu-01","ArcBox-Win2K19","ArcBox-Win2K25")
+
     $null = Connect-AzAccount -Identity -Tenant $env:spntenantId -Subscription $env:subscriptionId
 
 }
@@ -27,5 +27,15 @@ Describe "<vm>" -ForEach $VMs {
     It "Azure Arc Connected Machine is connected" {
         $connectedMachine = Get-AzConnectedMachine -Name $vm -ResourceGroupName $env:resourceGroup -SubscriptionId $env:subscriptionId
         $connectedMachine.Status | Should -Be "Connected"
+    }
+}
+
+
+Describe "Arc-enabled Servers" {
+    BeforeAll {
+        $ResourceGroupName = $env:resourceGroup
+    }
+    It "should have 3 resources or more" {
+        (Get-AzConnectedMachine -ResourceGroupName $env:resourceGroup -SubscriptionId $env:subscriptionId).count | Should -BeGreaterOrEqual 3
     }
 }
