e6fe96582dbda4cb5aba28ec6d9e1dc5925afd03
Jan Egil Ring <janegilring@microsoft.com>
2024-12-05 06:20:05 +0100

    Enhance Invoke-Test.ps1 to include scheduled task setup for automated test execution and improve output handling
    Signed-off-by: Jan Egil Ring <janegilring@microsoft.com>

--- azure_arc_servers_jumpstart/artifacts/tests/Invoke-Test.ps1
+++ azure_arc_servers_jumpstart/artifacts/tests/Invoke-Test.ps1
@@ -1,3 +1,5 @@
+#Requires -Modules @{ ModuleName="Pester"; ModuleVersion="5.6.0"}
+
 $Env:ArcBoxDir = "C:\ArcBox"
 $Env:ArcBoxTestsDir = "$Env:ArcBoxDir\Tests"
 
@@ -10,7 +12,7 @@ switch ($env:flavor) {
         Invoke-Pester -Path "$Env:ArcBoxTestsDir\devops.tests.ps1" -Output Detailed -PassThru -OutVariable tests_devops
         $tests_passed = $tests_passed + $tests_devops.Passed.Count
         $tests_failed = $tests_failed +  $tests_devops.Failed.Count
-}
+    }
     'DataOps' {
         Invoke-Pester -Path "$Env:ArcBoxTestsDir\dataops.tests.ps1" -Output Detailed -PassThru -OutVariable tests_dataops
         $tests_passed = $tests_passed + $tests_dataops.Passed.Count
@@ -20,31 +22,20 @@ switch ($env:flavor) {
         Invoke-Pester -Path "$Env:ArcBoxTestsDir\itpro.tests.ps1" -Output Detailed -PassThru -OutVariable tests_itpro
         $tests_passed = $tests_passed + $tests_itpro.Passed.Count
         $tests_failed = $tests_failed +  $tests_itpro.Failed.Count
-}
-    'Full' {
-        Invoke-Pester -Path "$Env:ArcBoxTestsDir\devops.tests.ps1" -Output Detailed -PassThru -OutVariable tests_devops
-        $tests_passed = $tests_passed + $tests_devops.Passed.Count
-        $tests_failed = $tests_failed +  $tests_devops.Failed.Count
-
-        Invoke-Pester -Path "$Env:ArcBoxTestsDir\dataops.tests.ps1" -Output Detailed -PassThru -OutVariable tests_dataops
-        $tests_passed = $tests_passed + $tests_dataops.Passed.Count
-        $tests_failed = $tests_failed +  $tests_dataops.Failed.Count
-
-        Invoke-Pester -Path "$Env:ArcBoxTestsDir\itpro.tests.ps1" -Output Detailed -PassThru -OutVariable tests_itpro
-        $tests_passed = $tests_passed + $tests_itpro.Passed.Count
-        $tests_failed = $tests_failed +  $tests_itpro.Failed.Count
     }
 }
 
 Write-Output "Tests succeeded: $tests_passed"
 Write-Output "Tests failed: $tests_failed"
 
-#Write-Header "Adding deployment test results to wallpaper using BGInfo"
+Write-Output "Adding deployment test results to wallpaper using BGInfo"
 
 Set-Content "$Env:windir\TEMP\arcbox-tests-succeeded.txt" $tests_passed
 Set-Content "$Env:windir\TEMP\arcbox-tests-failed.txt" $tests_failed
 
-#bginfo.exe $Env:ArcBoxTestsDir\arcbox-bginfo.bgi /timer:0 /NOLICPROMPT
+Set-JSDesktopBackground -ImagePath "$Env:ArcBoxDir\wallpaper.bmp"
+
+bginfo.exe $Env:ArcBoxTestsDir\arcbox-bginfo.bgi /timer:0 /NOLICPROMPT
 
 $DeploymentStatusPath = "C:\ArcBox\Logs\DeploymentStatus.log"
 
@@ -60,11 +51,15 @@ Write-Output "C:\ArcBox\Tests\Invoke-Test.ps1`n" | Out-File -FilePath $Deploymen
 
 Write-Output "Failed:" | Out-File -FilePath $DeploymentStatusPath -Append
 $tests_common.Failed | Out-File -FilePath $DeploymentStatusPath -Append
-$tests_k8s.Failed | Out-File -FilePath $DeploymentStatusPath -Append
+$tests_dataops.Failed | Out-File -FilePath $DeploymentStatusPath -Append
+$tests_devops.Failed | Out-File -FilePath $DeploymentStatusPath -Append
+$tests_itpro.Failed | Out-File -FilePath $DeploymentStatusPath -Append
 
 Write-Output "Passed:" | Out-File -FilePath $DeploymentStatusPath -Append
-$tests_k8s.Passed | Out-File -FilePath $DeploymentStatusPath -Append
 $tests_common.Passed | Out-File -FilePath $DeploymentStatusPath -Append
+$tests_dataops.Passed | Out-File -FilePath $DeploymentStatusPath -Append
+$tests_devops.Passed | Out-File -FilePath $DeploymentStatusPath -Append
+$tests_itpro.Passed | Out-File -FilePath $DeploymentStatusPath -Append
 
 Write-Header "Exporting deployment test results to resource group tag DeploymentStatus"
 
@@ -79,3 +74,37 @@ if ($null -ne $tags) {
 }
 
 $null = Set-AzResourceGroup -ResourceGroupName $env:resourceGroup -Tag $tags
+
+# Setup scheduled task for running tests on each logon
+$TaskName = "ArcBox Pester tests"
+$ActionScript = "C:\ArcBox\Tests\Invoke-Test.ps1"
+
+# Check if the scheduled task exists
+if (Get-ScheduledTask | Where-Object {$_.TaskName -eq $TaskName}) {
+    Write-Host "Scheduled task '$TaskName' already exists."
+} else {
+    # Create the task trigger
+    $Trigger = New-ScheduledTaskTrigger -AtLogOn
+
+    # Create the task action to use pwsh.exe
+    $Action = New-ScheduledTaskAction -Execute "pwsh.exe" -Argument "-NoProfile -ExecutionPolicy Bypass -File $ActionScript"
+
+    if ($env:flavor -eq "DataOps") {
+        $UserName = "jumpstart\" + $Env:UserName
+
+        # Rename the local user account to avoid scheduled task triggering issues
+        Rename-LocalUser -Name $Env:UserName -NewName "$($Env:UserName)_local"
+
+    } else {
+        $UserName = $Env:UserName
+    }
+
+    # Register the scheduled task for the current user
+    Register-ScheduledTask -TaskName $TaskName -Trigger $Trigger -Action $Action -User $UserName
+
+    Write-Host "Scheduled task $TaskName created successfully for the currently logged-on user, using pwsh.exe."
+
+    # logoff the user to apply the wallpaper in proper scaling and refresh tests results at first logon
+    logoff.exe
+
+}
