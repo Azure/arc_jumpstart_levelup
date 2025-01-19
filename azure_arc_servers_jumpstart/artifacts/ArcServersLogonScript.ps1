123f52f9aaf97f172860cc18046266b02bca7488
basimolimajeed <basim_majeed@yahoo.co.uk>
2025-01-19 15:56:14 +0000

    Update ArcServersLogonScript.ps1
    Change Autostar and Autoshutdown in Hyper-V VMs

--- azure_arc_servers_jumpstart/artifacts/ArcServersLogonScript.ps1
+++ azure_arc_servers_jumpstart/artifacts/ArcServersLogonScript.ps1
@@ -166,11 +166,23 @@ $serversDscConfigurationFile = "$Env:ArcBoxDscDir\virtual_machines_itpro.dsc.yml
 (Get-Content -Path $serversDscConfigurationFile) | Set-Content -Path $serversDscConfigurationFile
 winget configure --file C:\ArcBox\DSC\virtual_machines_itpro.dsc.yml --accept-configuration-agreements --disable-interactivity
 
-#Set-VM -Name $Win2k19vmName -AutomaticStartAction Start #-AutomaticStopAction ShutDown
-#Set-VM -Name $Win2k22vmName -AutomaticStartAction Start #-AutomaticStopAction ShutDown
-#Set-VM -Name $Ubuntu01vmName -AutomaticStartAction Start #-AutomaticStopAction ShutDown
-#Set-VM -Name $Ubuntu02vmName -AutomaticStartAction Start #-AutomaticStopAction ShutDown
-#Set-VM -Name $SQLvmName -AutomaticStartAction Start #-AutomaticStopAction ShutDown
+Set-VM -Name $Win2k19vmName -AutomaticStartAction Start -AutomaticStopAction ShutDown
+Set-VM -Name $Win2k22vmName -AutomaticStartAction Start -AutomaticStopAction ShutDown
+Set-VM -Name $Ubuntu01vmName -AutomaticStartAction Start -AutomaticStopAction ShutDown
+Set-VM -Name $Ubuntu02vmName -AutomaticStartAction Start -AutomaticStopAction ShutDown
+Set-VM -Name $SQLvmName -AutomaticStartAction Start -AutomaticStopAction ShutDown
+
+Start-Sleep -seconds 10
+
+# Start all the VMs
+Write-Host "Starting VMs"
+Start-VM -Name $Win2k19vmName
+Start-VM -Name $Win2k22vmName
+Start-VM -Name $Ubuntu01vmName
+Start-VM -Name $Ubuntu02vmName
+Start-VM -Name $SQLvmName
+
+Start-Sleep -seconds 10
 
 
 Write-Header "Creating VM Credentials"
